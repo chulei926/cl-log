d356b9bbef2f43864dd26433e653bf789bed817a
leichu <chulei926@126.com>
2020-06-13 09:42:19 +0800

    工程搭建

---
+++ cl-log-agent/src/main/java/com/cl/log/agent/ClLogAgentApplication.java
@@ -0,0 +1,13 @@
+package com.cl.log.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClLogAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClLogAgentApplication.class, args);
+	}
+
+}
---
+++ cl-log-server/src/main/java/com/cl/log/server/ClLogServerApplication.java
@@ -0,0 +1,13 @@
+package com.cl.log.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClLogServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClLogServerApplication.class, args);
+	}
+
+}
